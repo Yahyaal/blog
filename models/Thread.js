4113390157a6cd51d269271d0b87b432503bc12e
Brian <admin@john316mission.org>
2016-07-14 18:52:01 -0500

    Done

--- models/Thread.js
+++ models/Thread.js
@@ -3,7 +3,7 @@ var mongoose = require('mongoose');
 var Post = mongoose.model('Post', {		// Create a new model called Hit
 	title: String,
 	body: String,
-	date: String
+	date: Date
 });
 
 module.exports = Post;					// Export Hit
